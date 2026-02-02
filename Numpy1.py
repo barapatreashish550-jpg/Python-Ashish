86c795299c7aa780178991cc32ec00d0615b8e23
Ashish Barapatre <barapatreashish550@gmail.com>
2026-02-02 21:51:37 +0530

    Create Numpy1.py

---
+++ Numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr = np.array([[1, 2, 3],
+                [4, 5, 6]])
+
+
+print("Number of dimensions:", arr.ndim)
+print("Shape of the array:", arr.shape)
+print("Size of the array:", arr.size)
+print("Type of elements:", arr.dtype)
