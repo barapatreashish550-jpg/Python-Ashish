602f150f0d9562345e5d4a92b814b34ea0289de1
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-10 21:05:17 +0530

    program4.py

--- File/program4.py
+++ File/program4.py
@@ -1,17 +1,14 @@
-# Open source file in read mode
-source = open("source.txt", "r")
 
-# Open destination file in write mode
+source = open("source.txt", "r")
 destination = open("destination.txt", "w")
 
-# Read content from source file
+
 data = source.read()
 
-# Write content to destination file
 destination.write(data)
 
-# Close both files
 source.close()
 destination.close()
+
 
 print("File copied successfully.")
