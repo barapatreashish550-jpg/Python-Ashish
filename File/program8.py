256ca048dbea42324e859019ab8d1c79fc5a005b
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-10 21:02:28 +0530

    program8.py

--- File/program8.py
+++ File/program8.py
@@ -1,20 +1,17 @@
-# Open grades.txt in read mode
-file = open("grades.txt", "r")
 
-# Open passed.txt in write mode
+file = open("grades.txt", "r")
 passed = open("passed.txt", "w")
 
-# Read each line from the file
 for line in file:
-    data = line.split()      # Split name and grade
+    data = line.split()      
     name = data[0]
     grade = data[1]
 
     if grade == "A" or grade == "B":
         passed.write(name + "\n")
 
-# Close both files
 file.close()
 passed.close()
+
 
 print("Passed students written to passed.txt")
