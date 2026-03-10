1e94552161d935acfef1a4331d7c066c026d8818
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-10 21:05:47 +0530

    program3.py

--- File/program3.py
+++ File/program3.py
@@ -1,14 +1,12 @@
-# Open the file in read mode
+
 file = open("data.txt", "r")
 
-# Read the entire content of the file
 content = file.read()
 
-# Count the number of characters
+
 count = len(content)
 
-# Print the count
 print("Total number of characters:", count)
 
-# Close the file
+
 file.close()
