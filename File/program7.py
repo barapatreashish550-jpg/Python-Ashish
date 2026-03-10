d8a36da220ca05497b291ef99aa4e2c8095d5673
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-10 21:03:03 +0530

    program7.py

--- File/program7.py
+++ File/program7.py
@@ -1,15 +1,10 @@
-# Open the file in read mode
+
 file = open("words.txt", "r")
 
 count = 0
-
-# Read each line from the file
 for line in file:
-    words = line.lower().split()   # Convert to lowercase and split into words
+    words = line.lower().split()  
     count = count + words.count("the")
 
-# Close the file
 file.close()
-
-# Print the result
 print("The word 'the' appears", count, "times.")
