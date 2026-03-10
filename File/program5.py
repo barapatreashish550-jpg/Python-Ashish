cafb122002929198adbef0c8578213fd5e7f8930
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-10 21:04:12 +0530

    program5.py

--- File/program5.py
+++ File/program5.py
@@ -1,10 +1,7 @@
-# Open the file in append mode
+
 file = open("names.txt", "a")
 
-# Write the new name
 file.write("\nDavid")
-
-# Close the file
 file.close()
 
 print("Name added successfully.")
