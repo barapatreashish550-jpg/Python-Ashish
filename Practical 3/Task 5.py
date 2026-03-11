3093560e67f4afc7c1809d58cf84579bbd0cc1c3
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-11 21:14:33 +0530

    Add files via upload

---
+++ Practical 3/Task 5.py
@@ -0,0 +1,23 @@
+
+n = 5
+
+for i in range(1, n + 1):
+    # print leading spaces
+    print(" " * (n - i), end="")
+    
+    # print stars with two spaces
+    for j in range(i):
+        print("*  ", end="")
+    
+    print("\n")
+    
+# Output
+#   *  
+
+#   *  *  
+
+#   *  *  *  
+
+#  *  *  *  *  
+
+# *  *  *  *  *  
---
+++ Practical 3/Task 6.py
@@ -0,0 +1,32 @@
+n = 5
+
+# Upper half
+for i in range(1, n + 1):
+    for j in range(i):
+        print("* ", end="")
+    print("\n")
+
+# Lower half
+for i in range(n - 1, 0, -1):
+    for j in range(i):
+        print("* ", end="")
+    print("\n")
+    
+# Output
+# * 
+
+# * * 
+
+# * * * 
+
+# * * * * 
+
+# * * * * * 
+
+# * * * * 
+
+# * * * 
+
+# * * 
+
+# * 
