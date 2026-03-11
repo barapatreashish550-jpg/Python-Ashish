b30aac8099f36289327a9d9811988c9b538f26f4
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-11 21:24:39 +0530

    Add files via upload

---
+++ Practical 5/pr5 Assignment 2.py
@@ -0,0 +1,22 @@
+# a) Print the total number of items sold
+
+# b) Print the price of cheapest item sold
+
+# c) Print the price of costliest item sold
+
+# d) Print the price list in ascending order
+
+# e) Print the number of costliest items sold on the day"""
+
+prices = tuple(map(int, input("Enter item prices separated by space: ").split()))
+
+print("Total items sold:", len(prices))
+
+print("Cheapest item price:", min(prices))
+
+print("Costliest item price:", max(prices))
+
+print("Prices in ascending order:", tuple(sorted(prices)))
+
+costliest = max(prices)
+print("Number of costliest items sold:", prices.count(costliest))
