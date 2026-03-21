e40e2122ffb5ccdbbfbe7866849d7cde4af8118a
Ashish Barapatre <barapatreashish550@gmail.com>
2026-03-21 18:10:58 +0530

    Add files via upload

---
+++ PANDAS/Panda4.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+exam_data = {
+    'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily'],
+    'score': [12.5, 9, 16.5, np.nan, 9],
+    'attempts': [1, 3, 2, 3, 2],
+    'qualify': ['yes', 'no', 'yes', 'no', 'no']
+}
+
+labels = ['a', 'b', 'c', 'd', 'e']
+
+df = pd.DataFrame(exam_data, index=labels)
+
+filtered_data = df[df['attempts'] > 2]
+
+print(filtered_data)
